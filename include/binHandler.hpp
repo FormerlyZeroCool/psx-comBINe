a0b66c00b422aa94a29ad51400d0f2521248427e
andrewrubinstein <andrew.rubinstein93@gmail.com>
2023-05-06 01:58:01 -0400

    Change buffer size to 64mb

--- include/binHandler.hpp
+++ include/binHandler.hpp
@@ -22,8 +22,8 @@
 #include "helpers.hpp"
 #include "cueHandler.hpp"
 
-//Define how large the RAM byte array while dumping should be. (1mb)
-#define _def_ARR_SIZE 1<<20
+//Define how large the RAM byte array while dumping should be. (64mb)
+#define _def_ARR_SIZE 1<<26
 /** Variables *****************************************************************/
 //Each entity is at which byte a file transistion to the next file. This is for
 //.cue output INDEX value.
